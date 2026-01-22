6eac0f6a7ad681609d8fbab81be3fbb7cd3f7430
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-01-22 22:34:44 +0530

    Add implementation for twoSum function to find indices of two numbers that add up to a target

---
+++ neetcode-150/TwoPointers/TwoSumII.js
@@ -0,0 +1,13 @@
+const twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    if (numbers[left] + numbers[right] === target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
