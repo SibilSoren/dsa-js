e13bb238c5b5a2e2ddf59008d2b72fd6c371d99e
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-01-27 20:36:54 +0530

    Add implementation for dailyTemperatures function to calculate days until warmer temperatures

---
+++ neetcode-150/Stack/DailyTemperatures.js
@@ -0,0 +1,22 @@
+const dailyTemperatures = function (temperatures) {
+  const n = temperatures.length;
+  const result = new Array(n).fill(0);
+  const stack = []; // stores indices
+
+  for (let i = 0; i < n; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const prevIndex = stack.pop();
+      result[prevIndex] = i - prevIndex;
+    }
+
+    stack.push(i);
+  }
+
+  return result;
+};
+
+let temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
+console.log(dailyTemperatures(temperatures));
